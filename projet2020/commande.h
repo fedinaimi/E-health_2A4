7d370195208984a4735bf2da087507528af08c80
achrafbouslama <achraf.bouslama@esprit.tn>
2020-11-26 22:11:52 +0100

    first commit

---
+++ projet2020/commande.h
@@ -0,0 +1,28 @@
+#ifndef COMMANDE_H
+#define COMMANDE_H
+#include<QString>
+#include <QtSql/QSqlQueryModel>
+
+
+
+class commande
+{
+
+    QString num,nom , code,d;
+
+ public:
+    commande();
+    commande(QString ,QString,QString,QString);
+
+
+    bool ajouter();
+        QSqlQueryModel * afficher();
+    bool supprimer(QString);
+    bool modifier(QString, QString, QString,QString);
+        ~ commande(){}
+    int Calculcommandenum(QString num);
+};
+
+
+
+#endif // COMMANDE_H
