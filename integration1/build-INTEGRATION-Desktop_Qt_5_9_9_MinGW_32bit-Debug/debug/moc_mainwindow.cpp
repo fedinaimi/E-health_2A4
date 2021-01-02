c724fc328d6c0747251c4d7bcdc9cafe2715bbaa
ahmed bahrouni <ahmed.bahrouni@esprit.tn>
2021-01-02 21:51:11 +0100

    nouvelle mise à jour ajout de music ajout d'animation correction des bugs ajout de logo

---
+++ integration1/build-INTEGRATION-Desktop_Qt_5_9_9_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,350 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.9)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../integration/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.9. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[55];
+    char stringdata0[1323];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 24), // "on_pushButton_11_clicked"
+QT_MOC_LITERAL(2, 36, 0), // ""
+QT_MOC_LITERAL(3, 37, 24), // "on_pushButton_10_clicked"
+QT_MOC_LITERAL(4, 62, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(5, 84, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(6, 108, 35), // "on_lineEdit_6_cursorPositionC..."
+QT_MOC_LITERAL(7, 144, 4), // "arg1"
+QT_MOC_LITERAL(8, 149, 4), // "arg2"
+QT_MOC_LITERAL(9, 154, 22), // "on_tableView_activated"
+QT_MOC_LITERAL(10, 177, 5), // "index"
+QT_MOC_LITERAL(11, 183, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(12, 207, 36), // "on_lineEdit_15_cursorPosition..."
+QT_MOC_LITERAL(13, 244, 25), // "on_lineEdit_6_textChanged"
+QT_MOC_LITERAL(14, 270, 36), // "on_lineEdit_17_cursorPosition..."
+QT_MOC_LITERAL(15, 307, 30), // "on_lineEdit_6_selectionChanged"
+QT_MOC_LITERAL(16, 338, 23), // "on_pushButton_9_clicked"
+QT_MOC_LITERAL(17, 362, 20), // "on_tableView_clicked"
+QT_MOC_LITERAL(18, 383, 28), // "on_lineEdit_17_returnPressed"
+QT_MOC_LITERAL(19, 412, 23), // "on_pushButton_7_clicked"
+QT_MOC_LITERAL(20, 436, 35), // "on_lineEdit_7_cursorPositionC..."
+QT_MOC_LITERAL(21, 472, 25), // "on_lineEdit_7_textChanged"
+QT_MOC_LITERAL(22, 498, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(23, 522, 36), // "on_lineEdit_18_cursorPosition..."
+QT_MOC_LITERAL(24, 559, 29), // "on_tabWidget_2_currentChanged"
+QT_MOC_LITERAL(25, 589, 21), // "on_comboBox_activated"
+QT_MOC_LITERAL(26, 611, 23), // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(27, 635, 23), // "on_pushButton_6_clicked"
+QT_MOC_LITERAL(28, 659, 36), // "on_widget_customContextMenuRe..."
+QT_MOC_LITERAL(29, 696, 3), // "pos"
+QT_MOC_LITERAL(30, 700, 23), // "on_pushButton_8_clicked"
+QT_MOC_LITERAL(31, 724, 19), // "on_modifier_clicked"
+QT_MOC_LITERAL(32, 744, 26), // "on_lineEdit_21_textChanged"
+QT_MOC_LITERAL(33, 771, 24), // "on_pushButton_12_clicked"
+QT_MOC_LITERAL(34, 796, 24), // "on_pushButton_15_clicked"
+QT_MOC_LITERAL(35, 821, 24), // "on_pushButton_16_clicked"
+QT_MOC_LITERAL(36, 846, 26), // "on_lineEdit_26_textChanged"
+QT_MOC_LITERAL(37, 873, 24), // "on_pushButton_13_clicked"
+QT_MOC_LITERAL(38, 898, 24), // "on_pushButton_17_clicked"
+QT_MOC_LITERAL(39, 923, 24), // "on_pushButton_21_clicked"
+QT_MOC_LITERAL(40, 948, 24), // "on_pushButton_20_clicked"
+QT_MOC_LITERAL(41, 973, 26), // "on_lineEdit_31_textChanged"
+QT_MOC_LITERAL(42, 1000, 24), // "on_pushButton_22_clicked"
+QT_MOC_LITERAL(43, 1025, 24), // "on_pushButton_23_clicked"
+QT_MOC_LITERAL(44, 1050, 24), // "on_pushButton_24_clicked"
+QT_MOC_LITERAL(45, 1075, 24), // "on_pushButton_25_clicked"
+QT_MOC_LITERAL(46, 1100, 24), // "on_pushButton_29_clicked"
+QT_MOC_LITERAL(47, 1125, 24), // "on_pushButton_32_clicked"
+QT_MOC_LITERAL(48, 1150, 21), // "on_pb_ajouter_clicked"
+QT_MOC_LITERAL(49, 1172, 24), // "on_pushButton_34_clicked"
+QT_MOC_LITERAL(50, 1197, 24), // "on_pushButton_31_clicked"
+QT_MOC_LITERAL(51, 1222, 26), // "on_lineEdit_39_textChanged"
+QT_MOC_LITERAL(52, 1249, 26), // "on_lineEdit_41_textChanged"
+QT_MOC_LITERAL(53, 1276, 21), // "on_modifier_3_clicked"
+QT_MOC_LITERAL(54, 1298, 24) // "on_pushButton_33_clicked"
+
+    },
+    "MainWindow\0on_pushButton_11_clicked\0"
+    "\0on_pushButton_10_clicked\0"
+    "on_pushButton_clicked\0on_pushButton_3_clicked\0"
+    "on_lineEdit_6_cursorPositionChanged\0"
+    "arg1\0arg2\0on_tableView_activated\0index\0"
+    "on_pushButton_2_clicked\0"
+    "on_lineEdit_15_cursorPositionChanged\0"
+    "on_lineEdit_6_textChanged\0"
+    "on_lineEdit_17_cursorPositionChanged\0"
+    "on_lineEdit_6_selectionChanged\0"
+    "on_pushButton_9_clicked\0on_tableView_clicked\0"
+    "on_lineEdit_17_returnPressed\0"
+    "on_pushButton_7_clicked\0"
+    "on_lineEdit_7_cursorPositionChanged\0"
+    "on_lineEdit_7_textChanged\0"
+    "on_pushButton_4_clicked\0"
+    "on_lineEdit_18_cursorPositionChanged\0"
+    "on_tabWidget_2_currentChanged\0"
+    "on_comboBox_activated\0on_pushButton_5_clicked\0"
+    "on_pushButton_6_clicked\0"
+    "on_widget_customContextMenuRequested\0"
+    "pos\0on_pushButton_8_clicked\0"
+    "on_modifier_clicked\0on_lineEdit_21_textChanged\0"
+    "on_pushButton_12_clicked\0"
+    "on_pushButton_15_clicked\0"
+    "on_pushButton_16_clicked\0"
+    "on_lineEdit_26_textChanged\0"
+    "on_pushButton_13_clicked\0"
+    "on_pushButton_17_clicked\0"
+    "on_pushButton_21_clicked\0"
+    "on_pushButton_20_clicked\0"
+    "on_lineEdit_31_textChanged\0"
+    "on_pushButton_22_clicked\0"
+    "on_pushButton_23_clicked\0"
+    "on_pushButton_24_clicked\0"
+    "on_pushButton_25_clicked\0"
+    "on_pushButton_29_clicked\0"
+    "on_pushButton_32_clicked\0on_pb_ajouter_clicked\0"
+    "on_pushButton_34_clicked\0"
+    "on_pushButton_31_clicked\0"
+    "on_lineEdit_39_textChanged\0"
+    "on_lineEdit_41_textChanged\0"
+    "on_modifier_3_clicked\0on_pushButton_33_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      49,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  259,    2, 0x08 /* Private */,
+       3,    0,  260,    2, 0x08 /* Private */,
+       4,    0,  261,    2, 0x08 /* Private */,
+       5,    0,  262,    2, 0x08 /* Private */,
+       6,    2,  263,    2, 0x08 /* Private */,
+       9,    1,  268,    2, 0x08 /* Private */,
+      11,    0,  271,    2, 0x08 /* Private */,
+      12,    2,  272,    2, 0x08 /* Private */,
+      13,    1,  277,    2, 0x08 /* Private */,
+      14,    2,  280,    2, 0x08 /* Private */,
+      15,    0,  285,    2, 0x08 /* Private */,
+      16,    0,  286,    2, 0x08 /* Private */,
+      17,    1,  287,    2, 0x08 /* Private */,
+      18,    0,  290,    2, 0x08 /* Private */,
+      19,    0,  291,    2, 0x08 /* Private */,
+      20,    2,  292,    2, 0x08 /* Private */,
+      21,    1,  297,    2, 0x08 /* Private */,
+      22,    0,  300,    2, 0x08 /* Private */,
+      23,    2,  301,    2, 0x08 /* Private */,
+      24,    1,  306,    2, 0x08 /* Private */,
+      25,    1,  309,    2, 0x08 /* Private */,
+      26,    0,  312,    2, 0x08 /* Private */,
+      27,    0,  313,    2, 0x08 /* Private */,
+      28,    1,  314,    2, 0x08 /* Private */,
+      30,    0,  317,    2, 0x08 /* Private */,
+      31,    0,  318,    2, 0x08 /* Private */,
+      32,    1,  319,    2, 0x08 /* Private */,
+      33,    0,  322,    2, 0x08 /* Private */,
+      34,    0,  323,    2, 0x08 /* Private */,
+      35,    0,  324,    2, 0x08 /* Private */,
+      36,    1,  325,    2, 0x08 /* Private */,
+      37,    0,  328,    2, 0x08 /* Private */,
+      38,    0,  329,    2, 0x08 /* Private */,
+      39,    0,  330,    2, 0x08 /* Private */,
+      40,    0,  331,    2, 0x08 /* Private */,
+      41,    1,  332,    2, 0x08 /* Private */,
+      42,    0,  335,    2, 0x08 /* Private */,
+      43,    0,  336,    2, 0x08 /* Private */,
+      44,    0,  337,    2, 0x08 /* Private */,
+      45,    0,  338,    2, 0x08 /* Private */,
+      46,    0,  339,    2, 0x08 /* Private */,
+      47,    0,  340,    2, 0x08 /* Private */,
+      48,    0,  341,    2, 0x08 /* Private */,
+      49,    0,  342,    2, 0x08 /* Private */,
+      50,    0,  343,    2, 0x08 /* Private */,
+      51,    1,  344,    2, 0x08 /* Private */,
+      52,    1,  347,    2, 0x08 /* Private */,
+      53,    0,  350,    2, 0x08 /* Private */,
+      54,    0,  351,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    7,    8,
+    QMetaType::Void, QMetaType::QModelIndex,   10,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    7,    8,
+    QMetaType::Void, QMetaType::QString,    7,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    7,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QModelIndex,   10,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    7,    8,
+    QMetaType::Void, QMetaType::QString,    7,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    7,    8,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::QString,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QPoint,   29,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    7,
+    QMetaType::Void, QMetaType::QString,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_11_clicked(); break;
+        case 1: _t->on_pushButton_10_clicked(); break;
+        case 2: _t->on_pushButton_clicked(); break;
+        case 3: _t->on_pushButton_3_clicked(); break;
+        case 4: _t->on_lineEdit_6_cursorPositionChanged((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 5: _t->on_tableView_activated((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 6: _t->on_pushButton_2_clicked(); break;
+        case 7: _t->on_lineEdit_15_cursorPositionChanged((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 8: _t->on_lineEdit_6_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 9: _t->on_lineEdit_17_cursorPositionChanged((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+
+        case 11: _t->on_pushButton_9_clicked(); break;
+        case 12: _t->on_tableView_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 13: _t->on_lineEdit_17_returnPressed(); break;
+        case 14: _t->on_pushButton_7_clicked(); break;
+
+        case 16: _t->on_lineEdit_7_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 17: _t->on_pushButton_4_clicked(); break;
+        case 18: _t->on_lineEdit_18_cursorPositionChanged((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+
+        case 21: _t->on_pushButton_5_clicked(); break;
+        case 22: _t->on_pushButton_6_clicked(); break;
+
+        case 24: _t->on_pushButton_8_clicked(); break;
+        case 25: _t->on_modifier_clicked(); break;
+        case 26: _t->on_lineEdit_21_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 27: _t->on_pushButton_12_clicked(); break;
+        case 28: _t->on_pushButton_15_clicked(); break;
+        case 29: _t->on_pushButton_16_clicked(); break;
+        case 30: _t->on_lineEdit_26_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 31: _t->on_pushButton_13_clicked(); break;
+        case 32: _t->on_pushButton_17_clicked(); break;
+        case 33: _t->on_pushButton_21_clicked(); break;
+        case 34: _t->on_pushButton_20_clicked(); break;
+        case 35: _t->on_lineEdit_31_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 36: _t->on_pushButton_22_clicked(); break;
+        case 37: _t->on_pushButton_23_clicked(); break;
+        case 38: _t->on_pushButton_24_clicked(); break;
+        case 39: _t->on_pushButton_25_clicked(); break;
+        case 40: _t->on_pushButton_29_clicked(); break;
+        case 41: _t->on_pushButton_32_clicked(); break;
+        case 42: _t->on_pb_ajouter_clicked(); break;
+        case 43: _t->on_pushButton_34_clicked(); break;
+        case 44: _t->on_pushButton_31_clicked(); break;
+        case 45: _t->on_lineEdit_39_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 46: _t->on_lineEdit_41_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 47: _t->on_modifier_3_clicked(); break;
+        case 48: _t->on_pushButton_33_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 49)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 49;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 49)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 49;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
